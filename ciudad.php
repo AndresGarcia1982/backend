c6c03308289723a8e9514821b5be8bcc06131e8d
AndresGarcia1982 <54553973+AndresGarcia1982@users.noreply.github.com>
2021-05-20 10:35:22 -0500

    PrimerCodigo

---
+++ ciudad.php
@@ -0,0 +1,5 @@
+<?php
+  require('./componentes/libreria.php'); //Libreria requerida para el funcionamiento.
+  $getData = leerDatos(); //Lee los datos
+  obtnciudad($getData) //Ejecuta y obtiene la ciudad
+ ?>
